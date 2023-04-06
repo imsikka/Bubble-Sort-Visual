8c2af60e8e30bf8982366c80b891dc166096c988
Roshan Singh <72564554+imsikka@users.noreply.github.com>
2023-04-07 02:10:54 +0530

    Added no of swaps count

--- dist/script.js
+++ dist/script.js
@@ -2,21 +2,23 @@ function sort() {
   const input = document.getElementById("inputArray").value;
   const array = input.split(",").map((item) => parseInt(item.trim()));
   let len = array.length;
-  let i, j, stop;
+  let i, j, stop, swaps = 0;
   for (i = 0; i < len - 1; i++) {
     for (j = 0, stop = len - i - 1; j < stop; j++) {
       if (array[j] > array[j + 1]) {
         let temp = array[j];
         array[j] = array[j + 1];
         array[j + 1] = temp;
+        swaps++;
       }
-      visualize(array);
+      visualize(array, swaps);
     }
   }
+  document.getElementById("swaps").innerText = swaps;
   return array;
 }
 
-function visualize(array) {
+function visualize(array, swaps) {
   let result = document.getElementById("result");
   result.innerHTML = "";
   let maxNum = Math.max(...array);
@@ -35,4 +37,5 @@ function visualize(array) {
     barContainer.appendChild(label);
     result.appendChild(barContainer);
   }
+ 
 }
